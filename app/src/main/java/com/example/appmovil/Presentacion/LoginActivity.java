e667787383d3de38a5c11b707ed956616fc53099
nachobarzola <nbarzola@frsf.utn.edu.ar>
2020-02-05 14:47:44 -0300

    nachobarzola
    Se terminó la actividad grafica de Login. Falta la logica

---
+++ app/src/main/java/com/example/appmovil/Presentacion/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.appmovil.Presentacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import com.example.appmovil.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
