6a2302a9e7f792fb2daba72abbc57efbedb5c78f
nachobarzola <nbarzola@frsf.utn.edu.ar>
2020-02-07 23:10:34 -0300

    nachobarzola
    Se agregaron un par de cosas graficas a la actividad login.

---
+++ app/src/main/java/com/example/appmovil/Presentacion/CrearEventoActivity.java
@@ -0,0 +1,16 @@
+package com.example.appmovil.Presentacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appmovil.R;
+
+public class CrearEventoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_evento);
+    }
+}
