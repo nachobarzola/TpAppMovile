4388f6ee228ddac0707256a87f748ad7c7dd5de2
nachobarzola <nbarzola@frsf.utn.edu.ar>
2020-02-16 15:54:21 -0300

    nachobarzola
    Se agrego una parte de la logica de envio de invitacion por correo electronico.
    
     Falta la parte de adjuntar el codigo qr

--- app/src/main/java/com/example/appmovil/Adapter/EventoRecyclerAdapter.java
+++ app/src/main/java/com/example/appmovil/Adapter/EventoRecyclerAdapter.java
@@ -9,6 +9,8 @@ import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+
+import android.os.StrictMode;
 import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,6 +32,14 @@ import com.example.appmovil.ViewHolder.EventoViewHolder;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class EventoRecyclerAdapter extends RecyclerView.Adapter<EventoViewHolder> {
 
@@ -37,6 +48,12 @@ public class EventoRecyclerAdapter extends RecyclerView.Adapter<EventoViewHolder
     //Atributo usado como contador para saber que color poner en la barra lateral
     private Integer contadorColorBarraLateral=0;
     //-----
+    //Logica de correo
+    private Session session;
+    private String _CORREOAPP="controlEventosQR@gmail.com";
+    private String _CONTRASEÑACOREEOAPP="facunacho";
+    //-----
+
     public EventoRecyclerAdapter(List<Evento> eventos){
         this.listaEventos=eventos;
     }
@@ -67,7 +84,7 @@ public class EventoRecyclerAdapter extends RecyclerView.Adapter<EventoViewHolder
         //---------------BOTONES:
         holder.btnInvitar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(final View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
                 builder.setTitle("Ingresar correo del invitado");
 
@@ -86,9 +103,11 @@ public class EventoRecyclerAdapter extends RecyclerView.Adapter<EventoViewHolder
                         email = input.getText().toString();
 
                         Invitacion invitacion=new Invitacion(0,email,evento.getId(),"CREADA", new Date());
-
                         Log.d("invitacionCreada",String.valueOf(invitacion.getEventoId()));
                         InvitacionesRepository.getInstance().Guardar(invitacion,miHandler);
+                //Envio de la envitacion por mail
+                        enviarCorreoElectronico(email,"Beach Fest","Juan Blanco","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sullae consulatum? Non risu potius quam oratione eiciendum? Cum praesertim illa perdiscere ludus esset.","16/12/2020","02:00");
+
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -150,5 +169,51 @@ public class EventoRecyclerAdapter extends RecyclerView.Adapter<EventoViewHolder
                 contadorColorBarraLateral=0;
                 return Color.rgb(43,225,112);
         }
+    }
+
+    private void enviarCorreoElectronico(String correoReceptor,String nombreEvento,String nombreUsuarioCreadorEvento,String descripcionEvento,String fechaEvento,String horaEvento){
+        //La libreria usada es JavaMail de android
+
+        String msj_asunto="Envitación a participar del evento "+nombreEvento;
+        String msj_titulo="Usted ha sido invitado por: "+nombreUsuarioCreadorEvento;
+        String msj_cuerpo=msj_titulo+"<br><br><br>"+
+                descripcionEvento+"."+ "<br><br>" +
+                "Se realizara el día: "+fechaEvento+"."+ "<br>" +
+                "Hora: "+horaEvento+"."+ "<br>"+
+                "Codigo QR de ingreso al evento: <br>";
+        //<br> es el salto de linea
+
+
+        StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Properties properties= new Properties();
+        properties.put("mail.smtp.host","smtp.googlemail.com");
+        properties.put("mail.smtp.socketFactory.port","465");
+        properties.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.port","465");
+        try{
+            session= Session.getDefaultInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(_CORREOAPP,_CONTRASEÑACOREEOAPP);
+                }
+            });
+            if(session!=null){
+                javax.mail.Message message= new MimeMessage(session);
+                //Correo emisor
+                message.setFrom(new InternetAddress(_CORREOAPP));
+                //Asunto del mensaje
+                message.setSubject(msj_asunto);
+                //Correo receptor
+                message.setRecipients(javax.mail.Message.RecipientType.TO,InternetAddress.parse(correoReceptor));
+                //mensaje
+                message.setContent(msj_cuerpo,"text/html; charset=utf-8");
+                //enviamos correo
+                Transport.send(message);
+            }
+        }catch (Exception e){ }
+
+
     }
 }
